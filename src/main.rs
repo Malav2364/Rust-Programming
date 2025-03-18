ef92ace45ae7b14dbce521ab66f00c8f3d000ea3
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2025-03-19 00:13:29 +0530

    File adding and let and mut dtypes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// mod utils;
+mod helpers;
+fn main(){
+    let sum = helpers::math::add(40,50);
+    println!("sum: {}", sum);
+    let  a = 50;
+    print!("{}",a);
+    let a = 100;
+    print!("{}",a);
+    let mut  b = 10;
+    print!{"{}", b}
 }
